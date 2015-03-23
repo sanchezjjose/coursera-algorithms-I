fac336b8b548ee5d2f919064b1baa95c632b8484
Jose Sanchez <sanchezjjose@gmail.com>
2015-03-23 00:47:57 -0400

    8puzzle assignment

---
+++ 8puzzle/src/Board.java
@@ -0,0 +1,174 @@
+
+public class Board {
+
+  private int[] blocks1D;
+  private int N;
+  private int width; // the width of an array (i.e., a 3x5 array has width 3)
+
+  // construct a board from an N-by-N array of blocks
+  // (where blocks[i][j] = block in row i, column j)
+  public Board(int[][] blocks) {
+
+    /*****************
+
+        puzzle04.txt
+           3 x 3
+        ___________
+       |   |   |   |
+       | 2 | 3 | 5 |
+       |---|---|---|
+       | 1 | 0 | 4 |
+       |---|---|---|
+       | 7 | 8 | 6 |
+       |___|___|___|
+
+     ******************/
+
+    this.N = blocks.length;
+    this.width = blocks[0].length;
+    this.blocks1D = new int[N * N];
+
+    int k = 0;
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        this.blocks1D[k++] = blocks[i][j];
+      }
+    }
+  }
+
+  // board dimension N
+  public int dimension() {
+    return N;
+  }
+
+  // number of blocks out of place
+  public int hamming() {
+    int distance = 0;
+
+    for (int i = 0; i < blocks1D.length; i++) {
+      int blockIndex = i + 1;
+      int blockValue = blocks1D[i];
+
+      if (blockValue != 0 && blockValue != blockIndex) {
+        distance++;
+      }
+    }
+
+    return distance;
+  }
+
+  // sum of Manhattan distances between blocks and goal
+  public int manhattan() {
+    int distance = 0;
+
+    for (int i = 0; i < blocks1D.length; i++) {
+      int blockIndex = i + 1;
+      int blockValue = blocks1D[i];
+
+      if (blockValue != 0 && blockValue != blockIndex) {
+        Coordinates wrongPos = convert1Dto2D(blockIndex);
+        Coordinates rightPos = convert1Dto2D(blockValue);
+
+        int horizontalDistance = wrongPos.column - rightPos.column;
+        int verticalDistance = wrongPos.row - rightPos.row;
+
+        // sum of the vertical and horizontal distance
+        distance += Math.abs(verticalDistance) + Math.abs(horizontalDistance);
+      }
+    }
+
+    return distance;
+  }
+
+  // is this board the goal board?
+  public boolean isGoal() {
+    return false;
+  }
+
+  // a board that is obtained by exchanging two adjacent blocks in the same row
+  public Board twin() {
+    return null;
+  }
+
+  // does this board equal y?
+  public boolean equals(Object y) {
+    Board that = (Board) y;
+    return this.hashCode() == that.hashCode();
+  }
+
+  // all neighboring boards
+  public Iterable<Board> neighbors() {
+    return null;
+  }
+
+  // string representation of this board (in the output format specified below)
+  public String toString() {
+    StringBuilder s = new StringBuilder();
+    s.append(N + "\n");
+    for (int i = 1; i <= blocks1D.length; i++) {
+      s.append(String.format("%2d ", blocks1D[i - 1]));
+
+      if (i % N == 0) {
+        s.append("\n");
+      }
+    }
+    return s.toString();
+  }
+
+  private int convert2Dto1D(int row, int column) {
+    return (row - 1) * width + column;
+  }
+
+  private Coordinates convert1Dto2D(int index) {
+    Coordinates c = null;
+
+    if (index == 0) {
+      c = new Coordinates(N-1, N-1);
+
+    } else {
+//      int row = Math.abs(index / width);
+//      int col = Math.abs(index % width);
+
+      int k = 1;
+
+      // TODO: use equation (above) rather then traversal method
+      for (int i = 0; i < width; i++) {
+        for (int j = 0; j < width; j++) {
+          if (index == k) {
+            c = new Coordinates(i, j);
+          }
+          k++;
+        }
+      }
+    }
+
+    return c;
+  }
+
+  private class Coordinates {
+    private int row;
+    private int column;
+
+    public Coordinates(int row, int column) {
+      this.row = row;
+      this.column = column;
+    }
+  }
+
+  // unit tests (not graded)
+  public static void main(String[] args) {
+
+    int[][] initialBlocks = new int[][] {
+            {8,1,3},
+            {4,0,2},
+            {7,6,5}
+    };
+
+    Board board = new Board(initialBlocks);
+
+    System.out.println(board.toString());
+    System.out.println(board.hamming());
+    System.out.println(board.manhattan());
+  }
+}
---
+++ 8puzzle/src/Solver.java
@@ -0,0 +1,118 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Solver {
+
+  Board initial;
+  Stack<Board> solutionBoards = new Stack<Board>();
+
+  // find a solution to the initial board (using the A* algorithm)
+  public Solver(Board initial) {
+    this.initial = initial;
+  }
+
+  private class SearchNode implements Comparable<SearchNode> {
+    private Board board;
+    private SearchNode previousNode;
+    private int moves;
+
+    public SearchNode(Board board, SearchNode previousNode, int moves) {
+      this.board = board;
+      this.previousNode = previousNode;
+      this.moves = moves;
+    }
+
+    public Board getBoard() {
+      return board;
+    }
+
+    public SearchNode getPreviousNode() {
+      return previousNode;
+    }
+
+    public int getMoves() {
+      return moves;
+    }
+
+    public int getPriority() {
+      return board.manhattan();
+    }
+
+    @Override
+    public int compareTo(SearchNode that) {
+      return (int) Math.signum(this.getPriority() - that.getPriority());
+    }
+  }
+
+  // is the initial board solvable?
+  public boolean isSolvable() {
+    return false;
+  }
+
+  // min number of moves to solve initial board; -1 if unsolvable
+  public int moves() {
+    if (isSolvable()) {
+      return solutionBoards.size();
+    }
+
+    return -1;
+  }
+
+  // sequence of boards in a shortest solution; null if unsolvable
+  public Iterable<Board> solution() {
+    MinPQ<SearchNode> searchNodePQ = new MinPQ<SearchNode>();
+    Iterator<SearchNode> searchNodeItr = searchNodePQ.iterator();
+    Set<Integer> boardHashCodes = new HashSet<Integer>();
+
+    searchNodePQ.insert(new SearchNode(initial, null, 0));
+
+    while (searchNodeItr.hasNext()) {
+      SearchNode currentSearchNode = searchNodeItr.next();
+      SearchNode minSearchNode = searchNodePQ.delMin();
+
+      solutionBoards.push(minSearchNode.getBoard());
+      boardHashCodes.add(currentSearchNode.hashCode());
+
+      if (minSearchNode.getBoard().isGoal()) {
+        break;
+      }
+
+      for (Board b : currentSearchNode.getBoard().neighbors()) {
+        if (!boardHashCodes.contains(b.hashCode())) {
+          searchNodePQ.insert(new SearchNode(b, currentSearchNode, currentSearchNode.getMoves() + 1));
+        }
+      }
+    }
+
+    return solutionBoards;
+  }
+
+  // solve a slider puzzle (given below)
+  public static void main(String[] args) {
+
+    // create initial board from file
+    In in = new In(args[0]);
+    int N = in.readInt();
+    int[][] blocks = new int[N][N];
+    for (int i = 0; i < N; i++)
+      for (int j = 0; j < N; j++)
+        blocks[i][j] = in.readInt();
+
+    Board initial = new Board(blocks);
+
+    // solve the puzzle
+    Solver solver = new Solver(initial);
+
+    // print solution to standard output
+    if (!solver.isSolvable())
+      StdOut.println("No solution possible");
+    else {
+      StdOut.println("Minimum number of moves = " + solver.moves());
+      for (Board board : solver.solution())
+        StdOut.println(board);
+    }
+
+  }
+
+}
